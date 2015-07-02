82aae57afdebddb9025bf1149f463584c35b1785
profesooooor <profesooooor@gmail.com>
2015-07-02 10:46:44 +0200

    archivos jade

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -5,7 +5,7 @@ exports.question = function(req, res) {
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-	if(req.query.respuesta === 'Roma'){
+	if(req.query.respuesta.toUpperCase() === 'ROMA'){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 	} else {
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
