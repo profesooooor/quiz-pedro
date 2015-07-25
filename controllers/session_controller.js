d89d7c1c7ca97c63837493d910e6a5c2e131b75f
profesooooor <profesooooor@gmail.com>
2015-07-25 21:51:06 +0200

    Módulo 9. Ejercicio P2P: autologout
    No hago la parte opcional (estadísticas)

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,9 +1,23 @@
 // controllers/session_controller.js
+// req.session.inicio() lo estoy metiendo para controlar el tiempo de sesión para el ejercicio P2P
 
 exports.loginRequired = function(req, res, next) {
-    console.log(".......................... loginRequired ..................");
     if (req.session.user) {
-        next();
+        var t0, t1;
+        t0 = req.session.inicio;
+        t1 = new Date();
+        t1 = t1.getHours() * 3600 + t1.getMinutes() * 60 + t1.getSeconds();
+        console.log("Desde la última acción estos segundos han transcurrido: "+(t1-t0));
+        if (t1-t0<120) {
+            // Iniciamos de nuevo el cronómetro
+            var t;
+            t = new Date();
+            req.session.inicio = req.session.inicio = t.getHours() * 3600 + t.getMinutes() * 60 + t.getSeconds();
+            next();
+        } else {
+            console.log('Sesión inactiva durante más de 2 minutos - se cierra automáticamente');
+            res.redirect('/login');
+        }
     } else {
         res.redirect('/login');
     }
@@ -30,7 +44,10 @@ exports.create = function(req, res) {
         }
         // Crear req.session.user y guardar campos id y username
         // La sesión se define por la existencia de: req.session.user
+        var t;
         req.session.user = {id:user.id, username:user.username};
+        t = new Date();
+        req.session.inicio = t.getHours() * 3600 + t.getMinutes() * 60 + t.getSeconds();
         res.redirect(req.session.redir.toString()); // Redirección a path anterior a login (guardado en app.js)
     });
 };
